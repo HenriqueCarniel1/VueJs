95bcd40c3c92adcac345587c688da7df978d0b26
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-08-03 09:00:51 -0300

    first commit

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,21 +1,46 @@
 <template>
   <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
-  <p>Estou em busca de novas oportunidades!</p>
+  <p v-else>Estou em busca de novas oportunidades!</p>
   <p>Utilizo as seguintes tecnologias:</p>
   <ul>
     <li>JavaScript</li>
     <li>PHP</li>
     <li>Python</li>
   </ul>
+  <div>
+    <button @click="showEmail">Mostrar e-mail</button>
+    <p v-show="mostrar_email">mande um email para: {{ email }}</p>
+  </div>
   <p>Mande uma mensagem para: matheus@email.com</p>
+  <p>
+    para acessar meu portfolio
+    <a v-bind:href="meu_link" target="_blank">Basta clicar aqui</a>
+  </p>
+  <Picture />
 </template>
   <script>
+import Picture from "./Picture.vue";
+
 export default {
   name: "Info",
+
+  components: {
+    Picture,
+  },
+
   data() {
     return {
       esta_trabalhando: false,
+      mostrar_email: false,
+      email: "teste@gmail.com",
+      meu_link: "https://google.com",
     };
   },
+
+  methods: {
+    showEmail() {
+      this.mostrar_email = !this.mostrar_email
+    }
+  }
 };
 </script>
---
+++ src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+  <img :src="avatar" :alt="descricao" />
+</template>
+  
+  <script>
+export default {
+  name: "Picture",
+  data() {
+    return {
+      avatar: require("../assets/avatar.png"),
+      descricao: "Matheus Battisti",
+    };
+  },
+};
+</script>
+  
